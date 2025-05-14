48ea7cea46a99bfb5872380156c1ecbcbe1c6dab
menteora <menteora@gmail.com>
2025-05-14 21:41:31 +0200

    Update 1 file: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://menteora.github.io/blog-anticoach-astro/',
+base: '/blog-anticoach-astro/'
 	integrations: [mdx(), sitemap()],
 });
