20f3d8d9d9ff5e1c57c19d55902450efc83d1080
Luca D'Amico <menteora@gmail.com>
2025-05-14 22:14:43 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+ output: 'static',
 	site: 'https://menteora.github.io/blog-anticoach-astro/',
-  base: '/blog-anticoach-astro/',
+ base: '/blog-anticoach-astro/',
 	integrations: [mdx(), sitemap()],
 });
